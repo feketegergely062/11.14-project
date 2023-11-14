602404134f3319a4d9ec93f1fb52f9007adbaa45
gergely.fekete <gergely.fekete@gmail.com>
2023-11-14 15:05:09 +0100

    first commit

---
+++ src/app/base.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class BaseService {
+
+  serverURL="http://localhost:3000/doga"
+  constructor(private http:HttpClient) { }
+  
+
+  getPlants(){
+    return this.http.get(this.serverURL)
+  }
+}
